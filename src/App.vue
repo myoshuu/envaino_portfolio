f3f56cbc66e6f3a1c8b97128df96210f38939bd2
Joevano <myoshu.me@gmail.com>
2022-05-08 10:26:42 +0700

    Start Project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="mt-12 text-center border">
+      <header>
+        <div class="title text-lg text-slate-500">Hello 👋, It's Me</div>
+        <div class="sub-title text-2xl text-indigo-800 font-medium">
+          Edy Pangangkat
+        </div>
+        <div class="image w-40 mx-auto mt-8">
+          <img class="" src="@/assets/me.jpg" alt="" />
+        </div>
+      </header>
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Fredoka&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Fredoka", monospace;
+}
+
+.image {
+  img {
+    border-radius: 200rem;
+  }
 }
 </style>
